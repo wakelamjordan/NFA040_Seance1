cf3fd0d1f0b4ca91c060016b647317e6a13df901
jordan wakelam <jwakelams@gmail.com>
2024-12-16 13:45:25 +0100

    il me reste la maison à faire

---
+++ component/script/script.js
@@ -0,0 +1,22 @@
+const items = [
+  { tag: "a", href: "./index.html", text: "Index" },
+  { tag: "a", href: "./exercice1.html", text: "Exercice 1" },
+  { tag: "a", href: "./exercice2.html", text: "Exercice 2" },
+  { tag: "a", href: "./exercice3_4.html", text: "Exercice 3 & 4" },
+  { tag: "a", href: "./formulaire.html", text: "formulaire" },
+  { tag: "a", href: "./Jeu_Des_Base.html", text: "jeu de base" },
+  { tag: "a", href: "./maison_svg.html", text: "Maison svg" },
+];
+
+const ul = document.createElement("ul");
+
+for (let i = 0; i < items.length; i++) {
+  let item = document.createElement(items[i].tag);
+  item.setAttribute("href", items[i].href);
+  item.innerHTML = items[i].text;
+  let li = document.createElement("li");
+  li.appendChild(item);
+  ul.appendChild(li);
+}
+const navbar = document.getElementById("navbar");
+navbar.appendChild(ul)
